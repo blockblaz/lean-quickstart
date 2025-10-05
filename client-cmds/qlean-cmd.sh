0d4d022052d78c94731b4716480fe6d9d74b3562
turuslan <turuslan.devbox@gmail.com>
2025-10-05 10:00:10 +0500

    qlean docker

--- client-cmds/qlean-cmd.sh
+++ client-cmds/qlean-cmd.sh
@@ -12,7 +12,13 @@ node_binary="$scriptDir/qlean/build/src/executable/qlean \
       --node-id $item --node-key $configDir/$privKeyPath \
       --listen-addr /ip4/0.0.0.0/udp/$quicPort/quic-v1"
 
-node_docker=
+node_docker="--platform linux/amd64 qdrvm/qlean-mini:3f121fc \
+      --genesis /config/config.yaml \
+      --validator-registry-path /config/validators.yaml \
+      --bootnodes /config/nodes.yaml \
+      --data-dir /data \
+      --node-id $item --node-key /config/$privKeyPath \
+      --listen-addr /ip4/0.0.0.0/udp/$quicPort/quic-v1"
 
 # choose either binary or docker
-node_setup="binary"
+node_setup="docker"
