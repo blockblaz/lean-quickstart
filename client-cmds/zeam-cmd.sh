e3f0877948f9d600044f30c6c499a4ad6d06c995
ch4r10t33r <psramanuj@gmail.com>
2025-11-26 12:40:36 +0000

    fix: reverted to blockblaz/zeam:devnet1 image

--- client-cmds/zeam-cmd.sh
+++ client-cmds/zeam-cmd.sh
@@ -10,10 +10,7 @@ node_binary="$scriptDir/../zig-out/bin/zeam node \
       --node-id $item --node-key $configDir/$item.key \
       --metrics_port $metricsPort"
 
-# Use Zeam docker image (default: blockblaz/zeam:devnet1)
-# To use a local image, set ZEAM_DOCKER_IMAGE environment variable
-ZEAM_DOCKER_IMAGE="${ZEAM_DOCKER_IMAGE:-blockblaz/zeam:devnet1}"
-node_docker="--security-opt seccomp=unconfined $ZEAM_DOCKER_IMAGE node \
+node_docker="--security-opt seccomp=unconfined blockblaz/zeam:devnet1 node \
       --custom_genesis /config \
       --validator_config $validatorConfig \
       --data-dir /data \
