647b72812bbdf66b07bdb70b4a5d87142a7c1309
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-12-23 15:03:08 -0300

    chore: add ethlambda to client-cmds

---
+++ client-cmds/ethlambda-cmd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#-----------------------ethlambda setup----------------------
+
+binary_path="/Users/mega/lean_consensus/ethlambda/target/debug/ethlambda"
+
+# Command when running as binary
+node_binary="$binary_path"
+
+# Command when running as docker container
+# TODO: fill in docker command
+node_docker="--------------"
+
+node_setup="binary"
