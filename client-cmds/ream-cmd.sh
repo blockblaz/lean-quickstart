a1da7a69c5ae1bf9841fafceb6deb293ea8e9cfe
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2026-01-07 10:34:18 -0300

    Revert "fix: apply blockblaz/lean-quickstart#78"
    This reverts commit c4c1275489ced7d4caae934bda17d7ff5e6901f4.

--- client-cmds/ream-cmd.sh
+++ client-cmds/ream-cmd.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 #-----------------------ream setup----------------------
+devnet_flag=""
+if [ -n "$devnet" ]; then
+        devnet_flag="--devnet $devnet"
+fi
+
 # Metrics enabled by default
 metrics_flag="--metrics"
 
@@ -9,6 +14,7 @@ node_binary="$scriptDir/../ream/target/release/ream --data-dir $dataDir/$item \
         lean_node \
         --network $configDir/config.yaml \
         --validator-registry-path $configDir/validators.yaml \
+        $devnet_flag \
         --bootnodes $configDir/nodes.yaml \
         --node-id $item --node-key $configDir/$privKeyPath \
         --socket-port $quicPort \
@@ -21,6 +27,7 @@ node_docker="ghcr.io/reamlabs/ream:latest --data-dir /data \
         lean_node \
         --network /config/config.yaml \
         --validator-registry-path /config/validators.yaml \
+        $devnet_flag \
         --bootnodes /config/nodes.yaml \
         --node-id $item --node-key /config/$privKeyPath \
         --socket-port $quicPort \
