e25b368389a63818f349bdcc84a9ab9ad12e7bcc
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2026-01-14 20:53:29 -0300

    feat: add new flags

--- client-cmds/ethlambda-cmd.sh
+++ client-cmds/ethlambda-cmd.sh
@@ -7,8 +7,10 @@ binary_path="/Users/mega/lean_consensus/ethlambda/target/debug/ethlambda"
 # Command when running as binary
 node_binary="$binary_path \
       --custom-network-config-dir $configDir \
-      --gossipsub-port $quicPort
-"
+      --gossipsub-port $quicPort \
+      --node-key $configDir/$item.key \
+      --metrics-address 0.0.0.0 \
+      --metrics-port $metricsPort"
 
 # Command when running as docker container
 # TODO: fill in docker command
