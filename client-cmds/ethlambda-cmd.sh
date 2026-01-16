a516fe41e57df44a56156c1072a392659cd29afa
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2026-01-16 17:20:02 -0300

    feat: add docker command

--- client-cmds/ethlambda-cmd.sh
+++ client-cmds/ethlambda-cmd.sh
@@ -8,12 +8,18 @@ binary_path="/Users/mega/lean_consensus/ethlambda/target/debug/ethlambda"
 node_binary="$binary_path \
       --custom-network-config-dir $configDir \
       --gossipsub-port $quicPort \
+      --node-id $item \
       --node-key $configDir/$item.key \
       --metrics-address 0.0.0.0 \
       --metrics-port $metricsPort"
 
 # Command when running as docker container
-# TODO: fill in docker command
-node_docker="--------------"
+node_docker="ethlambda:latest \
+      --custom-network-config-dir /config \
+      --gossipsub-port $quicPort \
+      --node-id $item \
+      --node-key /config/$item.key \
+      --metrics-address 0.0.0.0 \
+      --metrics-port $metricsPort"
 
-node_setup="binary"
+node_setup="docker"
