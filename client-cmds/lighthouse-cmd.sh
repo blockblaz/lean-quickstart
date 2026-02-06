565eddd55f10e484b52ce1648448d1565b3d3aec
ch4r10t33r <psramanuj@gmail.com>
2026-02-06 14:56:05 +0000

    feat: add isAggregator flag to validator configuration
    Add support for configuring nodes as aggregators through validator-config.yaml.
    This allows selective designation of nodes to perform aggregation duties by
    setting isAggregator: true in the validator configuration.
    
    Changes:
    - Add isAggregator field (default: false) to all validators in both local and ansible configs
    - Update parse-vc.sh to extract and export isAggregator flag
    - Modify all client command scripts to pass --is-aggregator flag when enabled
    - Add isAggregator status to node information output

--- client-cmds/lighthouse-cmd.sh
+++ client-cmds/lighthouse-cmd.sh
@@ -3,6 +3,12 @@
 # Metrics enabled by default
 metrics_flag="--metrics"
 
+# Set aggregator flag based on isAggregator value
+aggregator_flag=""
+if [ "$isAggregator" == "true" ]; then
+    aggregator_flag="--is-aggregator"
+fi
+
 node_binary="$lighthouse_bin lean_node \
       --datadir \"$dataDir/$item\" \
       --config \"$configDir/config.yaml\" \
@@ -14,7 +20,8 @@ node_binary="$lighthouse_bin lean_node \
       --socket-port $quicPort\
       $metrics_flag \
       --metrics-address 0.0.0.0 \
-      --metrics-port $metricsPort"
+      --metrics-port $metricsPort \
+      $aggregator_flag"
 
 node_docker="hopinheimer/lighthouse:latest lighthouse lean_node \
       --datadir /data \
@@ -27,6 +34,7 @@ node_docker="hopinheimer/lighthouse:latest lighthouse lean_node \
       --socket-port $quicPort\
       $metrics_flag \
       --metrics-address 0.0.0.0 \
-      --metrics-port $metricsPort"
+      --metrics-port $metricsPort \
+      $aggregator_flag"
 
 node_setup="docker"
